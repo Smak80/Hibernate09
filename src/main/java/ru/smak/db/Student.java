316b0d5a8644b039aefd782203cd4bf5f03b4e92
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-01-21 20:28:39 +0300

    Добавление поддержки Hibernate

--- src/main/java/ru/smak/db/Student.java
+++ src/main/java/ru/smak/db/Student.java
@@ -1,4 +1,78 @@
 package ru.smak.db;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstname;
+    private String lastname;
+    private String group;
+    private Date birthday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Student(){}
+
+    public Student(
+        String firstname,
+        String lastname,
+        String group,
+        Date birthday
+    ){
+        id = 0;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.group = group;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString(){
+        return id + ". " + firstname + " " + lastname + ". " + group;
+    }
 }
